b4601ccf56fd0b18c04499f2db28e1158cc621c7
Opjasson <azmitheusos333@gmail.com>
2025-07-27 06:55:36 +0700

    backend : add transaksiId forgein_key

--- backend/models/keranjangModel.js
+++ backend/models/keranjangModel.js
@@ -2,7 +2,7 @@ import db from "../config/database.js";
 import { INTEGER, Sequelize } from "sequelize";
 import barang from "./productModel.js";
 import Users from "./user.js";
-// import transaksiModel from "./transaksiModel.js";
+import transaksiModel from "./transaksiModel.js";
 
 const cartModel = db.define(
     "keranjang",
@@ -25,7 +25,8 @@ cartModel.belongsTo(barang, { foreignKey: "productId" });
 
 Users.hasMany(cartModel);
 cartModel.belongsTo(Users, { foreignKey: "userId" });
-// transaksiModel.hasMany(cartModel);
-// cartModel.belongsTo(transaksiModel, { foreignKey: "transaksiId" });
+
+transaksiModel.hasMany(cartModel);
+cartModel.belongsTo(transaksiModel, { foreignKey: "transaksiId" });
 
 export default cartModel;
