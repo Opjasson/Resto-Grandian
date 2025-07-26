9c4d18a7689ff014bf7cab0ad79edeebe441df1d
Opjasson <azmitheusos333@gmail.com>
2025-07-26 16:57:26 +0700

    backend : done setting login controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,4 @@
+import Login from "../models/loginModel.js";
 import Users from "../models/user.js";
 import argon2 from "argon2";
 
@@ -13,6 +14,24 @@ export const login = async (req, res) => {
     if (!isValidPassword) {
         return res.status(401).json({ message: "password salah" });
     }
+
+    function generateRandomString(length) {
+        const characters =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(
+                Math.floor(Math.random() * characters.length)
+            );
+        }
+        return result;
+    }
+
+    const randomString = generateRandomString(10);
+    Login.create({
+        userId: user.id,
+        token: randomString,
+    });
     res.status(201).json({ message: "Login succesfully", response: user });
 };
 
