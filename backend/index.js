27be462996aa74eb1b95f488cdc3a1630d104681
Opjasson <azmitheusos333@gmail.com>
2025-07-27 06:39:38 +0700

    backend : add transaksi model

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import user_Route from "./routes/userRoute.js"
 import auth_Route from "./routes/authRoute.js"
 import dotenv from "dotenv";
 import cors from "cors";
-import transaksi from "./models/keranjangModel.js";
+import transaksi from "./models/transaksiModel.js";
 
 // (async() => {
 //     await transaksi.sync()
