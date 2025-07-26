04fa1fffa4e42615296e917eb5bbe23737ab0a76
Opjasson <azmitheusos333@gmail.com>
2025-07-26 09:02:39 +0700

    backend : done setting get products

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createproduct } from "../controllers/productController.js";
+import { createproduct, getproduct } from "../controllers/productController.js";
 
 const route = express.Router();
 
-// route.get("/barang", getBarang);
+route.get("/product", getproduct);
 // route.get("/barang/:id", getBarangById);
 route.post("/product", createproduct);
 // route.delete("/barang/:id", deleteBarangById);
