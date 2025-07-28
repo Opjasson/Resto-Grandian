e29959ee419d99994d9740751a9c05d5a1ce198d
Opjasson <azmitheusos333@gmail.com>
2025-07-28 11:52:13 +0700

    frontend : create Detail Transaksi Page

---
+++ frontend/app/pages/detailTransaksi/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const DetailTransaksi = () => {
+  return (
+    <View>
+      <Text>Detail Transaksi</Text>
+    </View>
+  )
+}
+
+export default DetailTransaksi
